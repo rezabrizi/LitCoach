205339e819e03e8088e5b6651ad7b2f0e464375f
Giridhar Nair <Giridhar.r.nair@gmail.com>
2025-01-31 12:45:08 -0600

    add endpoint to create a GitHub repository and enhance pagination for fetching user repos

--- api/routes/users.py
+++ api/routes/users.py
@@ -6,6 +6,7 @@ from api.utils.github import (
     get_user_repos,
     resolve_github_repo_id_to_repo_name,
     push_to_github,
+    create_github_repo,
 )
 
 
@@ -100,6 +101,26 @@ def submit_problem(request: LeetcodeSubmission):
             "problem": folder_name,
         }
 
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected Error: {str(e)}")
+    
+class CreateRepo(BaseModel):
+    github_id: int
+    repo_name: str
+    
+@router.post("/create_repo")
+def create_repo(request: CreateRepo):
+    try:
+        user = user_exists(request.github_id)
+        if not user:
+            raise HTTPException(status_code=403, details="User not found!")
+        repo_id = create_github_repo(request.repo_name, user.access_token)
+        return {
+            "message": "Repo created successfully!",
+            "repo_id": repo_id,
+        }
     except HTTPException as e:
         raise e
     except Exception as e:
